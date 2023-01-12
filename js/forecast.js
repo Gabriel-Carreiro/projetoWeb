3475f93ccc78c9bd0745113d2e75a887a7c6adc7
Camacho071098 <maggie_fcamacho98@hotmail.com>
2023-01-12 14:55:10 +0000

    Página Forecast V4

--- js/forecast.js
+++ js/forecast.js
@@ -3,7 +3,7 @@ var cloneFor = $("#forecast").clone();
 //console.log(cloneTemp);
 
 const apiKey = "82f4545f768cc3b045c97cca21bab0f3";
-var city = 'London';
+var city = 'Leiria';
 
 $("#lista").html("");
 $("#lista2").html("");
@@ -60,16 +60,30 @@ function data(msg,i) {
 }
 
 function data2(valor) {
-    let dt = new Date(valor * 1000).toDateString(); //timestamp * 1000
+    let dt = new Date(valor).toDateString(); //timestamp * 1000
 
     return dt.substring(0,4) + "| " + dt.substring(8,11) + dt.substring(4,7);
 }
 
+function get_hour(valor_data) {
+    let res = valor_data.toString();
+
+    return res.substring(16,18);
+}
+
+function get_day(valor_data) {
+    let res = valor_data.toDateString();
+
+    return res.substring(8,10);
+}
+
 function forecast_3h(element) {
     let clickedLI = $(element);
-    let data_value = clickedLI.children().first().text();
+    let data_value = parseInt(clickedLI.children().first().text() * 1000);
+    let atual_data = new Date(data_value);
 
-    //console.log(data_value);
+    console.log(data_value);
+    console.log(atual_data);
 
     $("#lista").css("display", "none");
     $("#lista2").css("display", "block");
@@ -81,76 +95,46 @@ function forecast_3h(element) {
     
         console.log(msg);
 
-        var liFor = cloneFor.clone();
+        var atual_hour, atual_temp, atual_hum;
 
-        //Valores Forecast 3 horas
         $("#data_atual", liFor).text(data2(data_value));
-
-        console.log(data_value);
-        //for(dia=0; dia<5; dia=+86400)
-        for(var i=0; i<msg.list.length; i++) {
-            //Manhã
-            if(msg.list[i].dt > data_value) {
-                if((msg.list[i].dt > msg.city.sunrise) && (msg.list[i].dt < msg.city.sunset)) {
-                    $("#temp2", liFor).text(parseInt(msg.list[i+1].main.temp.toFixed(0)));
-                    $("#hum2", liFor).text(parseInt(msg.list[i+1].main.humidity.toFixed(0)));
-
-                    console.log("Manhã");
-                    console.log(msg.list[i].dt);
-                    console.log(i);
-                    //console.log(data(msg));
-
-                    break;
-                }
-
-                else {
-                    $("#temp2", liFor).text("-");
-                    $("#hum2", liFor).text("-");
-                }
-            }
-        }
-
-        for(i=0; i<msg.list.length; i++) {
-            //Tarde
-            if(msg.list[i].dt > data_value) {
-                if((msg.list[i].dt > ((msg.city.sunrise + msg.city.sunset)/2)) && (msg.list[i].dt > msg.city.sunrise) && (msg.list[i].dt < msg.city.sunset)) {
-                    $("#temp3", liFor).text(parseInt(msg.list[i].main.temp.toFixed(0)));
-                    $("#hum3", liFor).text(parseInt(msg.list[i].main.humidity.toFixed(0)));
-
-                    console.log("Tarde");
-                    console.log(msg.list[i].dt);
-                    console.log(i);
-                    //console.log(data(msg));
-
-                    break;
-                }
-
-                else {
-                    $("#temp3", liFor).text("-");
-                    $("#hum3", liFor).text("-");
-                }
-            }
-        }
-
-        for(i=0; i<msg.list.length; i++) {
-            //Noite
-            if(msg.list[i].dt > data_value) {
-                if(msg.list[i].dt > msg.city.sunset) {
-                    $("#temp4", liFor).text(parseInt(msg.list[i].main.temp.toFixed(0)));
-                    $("#hum4", liFor).text(parseInt(msg.list[i].main.humidity.toFixed(0)));
-
-                    console.log("Noite");
-                    console.log(msg.list[i].dt);
-                    console.log(i);
-                    //console.log(data(msg));
-
-                    break;
-                }
-            }
-        }
-
         $("#tempo2", liFor).attr("src", "http://openweathermap.org/img/wn/" + msg.list[0].weather[0].icon + ".png");
 
+        for(var i=0; i<msg.list.length; i++) {
+            var liFor = cloneFor.clone();
+            let atual_day = get_day(atual_data); 
+
+            if(atual_day == get_day(new Date(msg.list[i].dt * 1000))) {
+                let atual_hour = get_hour(new Date(msg.list[i].dt * 1000));
+                let atual_temp = parseInt(msg.list[i].main.temp);
+                let atual_hum = parseInt(msg.list[i].main.humidity);
+
+                console.log(i);
+                console.log(get_day(atual_data) + "atual_data");
+                console.log(get_day(new Date(msg.list[i].dt * 1000)) + "data [" + i + "]");
+                console.log(atual_temp);
+                console.log(atual_hour);
+                console.log(atual_hum);
+
+                //Valores Forecast 3 horas
+                $("#hour2", liFor).text(atual_hour + ":00");
+                $("#temp2", liFor).text(atual_temp + "°C");
+                $("#hum2", liFor).text(atual_hum + " %");
+
+                //Atualizar os valores no html
+                $("#lista2").append(liFor);
+                
+                atual_day=+3;
+            }
+            else {
+                //Valores Forecast 3 horas
+                $("#hour2", liFor).text("-");
+                $("#temp2", liFor).text("-");
+                $("#hum2", liFor).text("-");
+            }
+        }
+
+        //$("#lista2").append(liFor);
         $("#lista2").append(liFor);
 
         //console.log($("#data").text());
