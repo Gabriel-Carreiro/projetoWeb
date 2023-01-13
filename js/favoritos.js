dd9bd97accc41144ada8717b2b0d711651810ff1
Camacho071098 <maggie_fcamacho98@hotmail.com>
2023-01-13 20:52:59 +0000

    Página Favoritos a funcionar

--- js/favoritos.js
+++ js/favoritos.js
@@ -1,32 +1,49 @@
 'use strict';
+let stored_data = obter();
+let stored_data_size = Object.keys(stored_data).length;
+let apiKey = "82f4545f768cc3b045c97cca21bab0f3";
+let cloneTemp = $("#temp").clone();
+$("#lista").html("");
 
-if (typeof(Storage) != "undefined") {
-    let get_new_data = localStorage.getItem('nome');
-    let get_old_data = localStorage.getItem('old_data');
+//alert(stored_data);
+//console.log(stored_data[1]);
 
-    if(get_new_data != null) {
-        //Se não houver nenhum valor no localstorage
-        if(get_old_data == null) {
-            localStorage.setItem('old_data', "[]");
-            get_old_data = localStorage.getItem('old_data');
-        }
+for(var i=0; i<stored_data_size; i++) {
+    //alert("Passou aqui dentro");
+    
+    $.ajax({
+        method: "GET",
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + stored_data[i] + "&units=metric&appid=" + apiKey + "&lang=pt"
+    }).done(function(msg) {
+        //alert("Processo GET terminado!");
 
-        else {
-            localStorage.setItem('old_data', get_new_data);
-        }
+        console.log(msg);
 
-        alert(get_old_data);
+        var liTemp = cloneTemp.clone();
 
-        let old_data = JSON.parse(get_old_data);   
-        old_data.push(get_new_data);
-        localStorage.setItem('old_data', JSON.stringify(old_data));
-        
-        alert("Passou aqui!");
+        $("#cidade", liTemp).text(msg.name);
+        $("#tempo", liTemp).attr("src", "http://openweathermap.org/img/wn/" + msg.weather[0].icon + ".png");
+        $("#temperatura", liTemp).text(parseInt(msg.main.temp_max) + " / " + parseInt(msg.main.temp_min) + "°C");
+        $("#vento2").text(parseFloat(msg.wind.speed.toFixed(1)) + " m/s");
+        $("#hum2").text(parseInt(msg.main.humidity) + " %");
 
+        //Atualizar os valores no html
+        $("#lista").append(liTemp);
+    });
+};
+
+function obter() {
+    if (typeof(Storage) != "undefined") {
+        //Passar do localstorage para um variável
+        let get_stored_data =  localStorage.getItem('old_data');
+
+        return JSON.parse(get_stored_data);
     }
-}
 
-else {
-    alert("Não é válido!");
-    event.preventDefault();
+    else {
+        alert("Não é válido!");
+        event.preventDefault();
+
+        return null;
+    }
 }
--- js/home.js
+++ js/home.js
@@ -24,6 +24,7 @@ function procurar(elemento) {
         event.preventDefault();
     }
     city_value();
+    salvar();
 }
 
 function home_list() {
@@ -79,4 +80,37 @@ function city_value() {
 function home2() {
     $("#card").css("display", "block");
     $("#card2").css("display", "none");
+}
+
+function salvar() {
+    if (typeof(Storage) != "undefined") {
+        let get_new_data = localStorage.getItem('nome');
+        let get_old_data = localStorage.getItem('old_data');
+
+        if(get_new_data != null) {
+            //Se não houver nenhum valor no localstorage
+            if(get_old_data == null) {
+                localStorage.setItem('old_data', "[]");
+                get_old_data = localStorage.getItem('old_data');
+            }
+
+            else {
+                localStorage.setItem('old_data', get_new_data);
+            }
+
+            alert(get_old_data);
+
+            let old_data = JSON.parse(get_old_data);   
+            old_data.push(get_new_data);
+            localStorage.setItem('old_data', JSON.stringify(old_data));
+            
+            //alert("Passou aqui!");
+
+        }
+    }
+
+    else {
+        alert("Não é válido!");
+        event.preventDefault();
+    }
 }
